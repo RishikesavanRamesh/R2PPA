77783b637785c4cb4b364516e14cb46a075d3054
bot <root@buildkitsandbox.earthly>
2025-04-13 05:02:48 +0000

    Automated gh-page build

---
+++ r2ppa-site/src/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Instruction from "./components/Instruction";
+import PackageTable from "./components/PackageTable"
+
+import { Boxes } from 'lucide-react';
+import Footer from "./components/Footer";
+
+
+
+const App: React.FC = () => {
+  return (
+    
+    <div className="select-none h-screen w-screen flex flex-col justify-center items-center overflow-scroll scrollbar-hidden focus:outline-none focus:ring-2 focus:ring-gray-300">
+      <div className="w-[80vw] h-full flex flex-col">
+        
+
+      <div className="flex flex-row items-end gap-2 p-1 justify-between">
+
+        <div className="flex flex-row items-center gap-2">
+  <Boxes size={30} />
+  <div className="text-3xl uppercase">r2ppa</div> </div>
+  <div className="text-red-500 ">
+    This PPA is intended for <a href="https://github.com/RishikesavanRamesh/" className="text-purple-600 underline"             target="_blank" 
+            rel="noopener noreferrer" >my</a> personal use, but if you're interested, proceed with caution!
+  </div>
+</div>
+        <div className="flex flex-col gap-y-3 h-full scrollbar-hidden">
+          <Instruction />
+
+          <PackageTable/>
+
+
+        <Footer/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
